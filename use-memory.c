d595553ed278c47b2839bfb932e905f98dfdfe1c
Patrick Selin <114651781+patrick-selin@users.noreply.github.com>
2024-09-12 20:42:34 +0300

    Create use-memory.c

---
+++ use-memory.c
@@ -0,0 +1,4 @@
+# Take one command line argument: The number of megabytes of memory it will use.
+# allocate an array and constantly loop through the array, accessing each entry.
+# The program should do this indefinitely.
+
