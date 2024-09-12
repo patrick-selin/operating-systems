5af23dc3785876e33f991c2d6230c21642220265
Patrick Selin <114651781+patrick-selin@users.noreply.github.com>
2024-09-12 20:51:10 +0300

    Update mmap.c

--- mmap.c
+++ mmap.c
@@ -1,2 +1,23 @@
 # Allocate system memory with mmap().
 # Establish a region of anonymous memory, which can be treated like a heap.
+
+#include <stdio.h>
+#include <sys/mman.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    #define PAGESIZE 4096
+  
+        void *pointer1 = mmap (NULL, 40 * PAGESIZE,
+            PROT_READ | PROT_WRITE,
+            MAP_PRIVATE | MAP_ANONYMOUS, 0, 0 );
+
+    void *pointer2 = mmap (NULL, PAGESIZE,
+            PROT_READ | PROT_WRITE,
+            MAP_PRIVATE | MAP_ANONYMOUS, 0, 0 );
+  
+    printf("First page: %p\n", pointer1);
+    printf("Second page: %p\n", pointer2);
+  
+    return 0;
+}
