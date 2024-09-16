8dae6512c4050355d3b10bc581b3cb9ad0260db0
Patrick Selin <114651781+patrick-selin@users.noreply.github.com>
2024-09-16 19:19:24 +0300

    Update segmentation.c

--- segmentation.c
+++ segmentation.c
@@ -1,1 +1,10 @@
-
+// get top 2 bits of 14-bit VA
+Segment = (VirtualAddress & SEG_MASK) >> SEG_SHIFT
+  
+// get offset
+Offset  = VirtualAddress & OFFSET_MASK
+if (Offset >= Bounds[Segment])
+    RaiseException(PROTECTION_FAULT)
+else
+    PhysAddr = Base[Segment] + Offset
+    Register = AccessMemory(PhysAddr)
