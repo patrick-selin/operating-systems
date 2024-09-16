3235ec21f124c82b2c88c856608cb229cc71ecc6
Patrick Selin <114651781+patrick-selin@users.noreply.github.com>
2024-09-16 19:24:32 +0300

    Update my-malloc.c

--- my-malloc.c
+++ my-malloc.c
@@ -1,1 +1,20 @@
+#include<stdio.h>
+#include<stddef.h>
+#include "mymalloc.h"
 
+
+void initialize(){
+ freeList->size=20000-sizeof(struct block);
+ freeList->free=1;
+ freeList->next=NULL;
+}
+
+void split(struct block *fitting_slot,size_t size){
+ struct block *new=(void*)((void*)fitting_slot+size+sizeof(struct block));
+ new->size=(fitting_slot->size)-size-sizeof(struct block);
+ new->free=1;
+ new->next=fitting_slot->next;
+ fitting_slot->size=size;
+ fitting_slot->free=0;
+ fitting_slot->next=new;
+}
