ffb31f86be7122c8b65ce75d2291937285cd6c61
Patrick Selin <114651781+patrick-selin@users.noreply.github.com>
2024-09-19 12:23:08 +0300

    Update disk-scheduling-scan.c

--- disk-scheduling-scan.c
+++ disk-scheduling-scan.c
@@ -1,1 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define MAX 199
+#define MIN 0
+
+void sort(int arr[], int n) {
+    int temp;
+    for (int i = 0; i < n-1; i++) {
+        for (int j = i+1; j < n; j++) {
+            if (arr[i] > arr[j]) {
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+}
 
