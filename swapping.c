e321d27978f0fcb1b3f9731b40e1a28123b42231
Patrick Selin <114651781+patrick-selin@users.noreply.github.com>
2024-09-19 11:48:38 +0300

    Update swapping.c

--- swapping.c
+++ swapping.c
@@ -1,1 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
+int main(int argc, char** argv) {
+    int max = -1;
+    int bytesInMB = 1048576;
+
+    if(argc > 1)
+        max = atoi(argv[1]);
+
+    int counter = 0;
+    char* address;
+    
+    while(counter != max) {
+        address = malloc(bytesInMB);
+
+        if(address != NULL) {
+            memset(address, 0, bytesInMB);
+        }
+        counter++;
+
+        if(counter%100==0){
+            printf("Allocated %d MB\n", counter);
+        }
+    }
+    return 0;
+}
