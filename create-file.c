e6594941992417bc4c566cd7f3f99027f99efa4a
Patrick Selin <114651781+patrick-selin@users.noreply.github.com>
2024-09-20 10:36:44 +0300

    Update create-file.c

--- create-file.c
+++ create-file.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h> 
+
+int main()
+{
+  int fd = open("aFile", O_CREAT | O_WRONLY | O_TRUNC,  S_IRUSR | S_IWUSR);
+  
+  printf("The file descriptor of the file is: %d\n", fd);
+  return 0;
+}
 
