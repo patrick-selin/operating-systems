8a258a795a837cb6c89e397d12a0c310f49be3e1
Patrick Selin <114651781+patrick-selin@users.noreply.github.com>
2024-09-20 11:12:19 +0300

    Update fork-seek.c

--- fork-seek.c
+++ fork-seek.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <assert.h>
 
+int main(int argc, char *argv[]) {
+    int fd = open("essay.txt", O_CREAT | O_RDONLY);
+    assert(fd >= 0);
+    int rc = fork();  
+  
+    if (rc == 0) {
+        rc = lseek(fd, 10, SEEK_SET);
+        printf("child offset = %d\n", rc);
+    } else if (rc > 0) {
+        (void) wait(NULL);
+        printf("parent offset = %d\n",
+               (int) lseek(fd, 0, SEEK_CUR));
+ }
+  
+return 0; 
+}
