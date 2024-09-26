f2e78375b218b5b4f6efc39235105c528cb491db
Patrick Selin <114651781+patrick-selin@users.noreply.github.com>
2024-09-26 10:10:52 +0300

    Update server.c

--- server.c
+++ server.c
@@ -1,1 +1,31 @@
+# UDP server
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+#define PORT 8080
+#define MAXLINE 1024
+
+int main() {
+    int sockfd;
+    char buffer[MAXLINE];
+    char *response = "I am fine, thank you.";
+    struct sockaddr_in servaddr, cliaddr;
+
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&servaddr, 0, sizeof(servaddr));
+    memset(&cliaddr, 0, sizeof(cliaddr));
+
+    close(sockfd);
+    return 0;
+}
